af8b56cf7b8eeae39e84103129d2a00fba51442f
shimon-d <sdegli@proton.me>
2023-04-14 22:54:41 -0300

    beginning lisp journey

---
+++ lisp/8kyu/you-cant-code-under-pressure-1.lisp
@@ -0,0 +1,7 @@
+; https://www.codewars.com/kata/53ee5429ba190077850011d4/train/commonlisp
+
+(defun double-integer (n)
+  ; n will be an integer. Double it and return it.
+  (* n 2))
+
+(print (double-integer 5))
