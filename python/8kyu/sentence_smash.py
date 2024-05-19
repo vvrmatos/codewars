9cdecf2985d6270dfe0ff664e488fe7df6abb995
vvrmatos <vvrmatos@proton.me>
2024-05-18 21:36:43 -0300

    strings, arrays, fundamentals

---
+++ python/8kyu/sentence_smash.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/python
+
+def smash(words: list):
+    return ' '.join('words)
+
+# e.g
+# ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
