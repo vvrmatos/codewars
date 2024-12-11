a402e0da3e21aa146cca47a9c7652bbc823f8b55
vvrmatos <victorkolis@duck.com>
2024-12-10 23:42:33 -0600

    initial lua commit

---
+++ python/8kyu/0017_miles_per_gallon_to_kilometers_per_liter/app/solution_1.py
@@ -0,0 +1,19 @@
+def add(a, b):
+	return a + b
+
+
+print(add(1, 7))
+
+
+def function(name):
+	add_to_data_base = True
+#	return {"msg": "the data has been saved"}
+	
+print(function("Joseph"))
+
+
+displayedLetter = None
+if detect:
+	displayedLetter = "A"
+else:
+	displayedLetter = str
